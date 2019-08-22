1a4a2312791ac274528e57feeda4f572de260b0d
Min0819 <mf3200@columbia.edu>
2019-08-22 14:18:47 +0800

    aaa

--- src/backend.py
+++ src/backend.py
@@ -4,6 +4,7 @@ from flask import request
 from flask import send_file
 import tensorflow as tf
 import numpy as np
+import copy
 
 
 app = Flask(__name__)
@@ -25,15 +26,25 @@ def parse_req():
     print(requestedData)
     result = {}
 
-    result["circle"] = requestedData["circle"]*2 - 1
-    result["square"] = requestedData["square"]*2 - 1
-    result["triangle"] = requestedData["triangle"]*2 - 1
-    result["bright_dark"] = requestedData["bright_dark"]*2 - 1
-    result["soft_sharp"] = requestedData["soft_sharp"]*2 - 1
-    result["warm_cool"] = requestedData["warm_cool"]*2 - 1
-    result["simple_complex"] = requestedData["simple_complex"]*2 - 1
-    result["disorder_inorder"] = requestedData["disorder_inorder"]*2 - 1
-    result["high_low"] = requestedData["high_low"]*2 - 1
+    # result["circle"] = requestedData["circle"]*2 - 1
+    # result["square"] = requestedData["square"]*2 - 1
+    # result["triangle"] = requestedData["triangle"]*2 - 1
+    # result["bright_dark"] = requestedData["bright_dark"]*2 - 1
+    # result["soft_sharp"] = requestedData["soft_sharp"]*2 - 1
+    # result["warm_cool"] = requestedData["warm_cool"]*2 - 1
+    # result["simple_complex"] = requestedData["simple_complex"]*2 - 1
+    # result["disorder_inorder"] = requestedData["disorder_inorder"]*2 - 1
+    # result["high_low"] = requestedData["high_low"]*2 - 1
+
+    result["circle"] = requestedData["circle"]
+    result["square"] = requestedData["square"]
+    result["triangle"] = requestedData["triangle"]
+    result["bright_dark"] = requestedData["bright_dark"]
+    result["soft_sharp"] = requestedData["soft_sharp"]
+    result["warm_cool"] = requestedData["warm_cool"]
+    result["simple_complex"] = requestedData["simple_complex"]
+    result["disorder_inorder"] = requestedData["disorder_inorder"]
+    result["high_low"] = requestedData["high_low"]
 
     return result
 
@@ -44,21 +55,30 @@ def parse_req_1():
 
     result = {}
 
-    result["circle"] = requestedData["circle"]*2 - 1
-    result["square"] = requestedData["square"]*2 - 1
-    result["triangle"] = requestedData["triangle"]*2 - 1
-    result["bright_dark"] = requestedData["bright_dark"]*2 - 1
-    result["soft_sharp"] = requestedData["soft_sharp"]*2 - 1
-    result["warm_cool"] = requestedData["warm_cool"]*2 - 1
-    result["simple_complex"] = requestedData["simple_complex"]*2 - 1
-    result["disorder_inorder"] = requestedData["disorder_inorder"]*2 - 1
-    result["high_low"] = requestedData["high_low"]*2 - 1
+    # result["circle"] = requestedData["circle"]*2 - 1
+    # result["square"] = requestedData["square"]*2 - 1
+    # result["triangle"] = requestedData["triangle"]*2 - 1
+    # result["bright_dark"] = requestedData["bright_dark"]*2 - 1
+    # result["soft_sharp"] = requestedData["soft_sharp"]*2 - 1
+    # result["warm_cool"] = requestedData["warm_cool"]*2 - 1
+    # result["simple_complex"] = requestedData["simple_complex"]*2 - 1
+    # result["disorder_inorder"] = requestedData["disorder_inorder"]*2 - 1
+    # result["high_low"] = requestedData["high_low"]*2 - 1
+
+    result["circle"] = requestedData["circle"]
+    result["square"] = requestedData["square"]
+    result["triangle"] = requestedData["triangle"]
+    result["bright_dark"] = requestedData["bright_dark"]
+    result["soft_sharp"] = requestedData["soft_sharp"]
+    result["warm_cool"] = requestedData["warm_cool"]
+    result["simple_complex"] = requestedData["simple_complex"]
+    result["disorder_inorder"] = requestedData["disorder_inorder"]
+    result["high_low"] = requestedData["high_low"]
 
     # it depends on the type of data from front end
     # now it just for test on postman
     # result["random_noise"] = [float(x) for x in request.args.get("random_noise")[1:-1].split(",")]
     result["random_noise"] = requestedData["random_noise"]
-
     return result
 
 @app.route("/test", methods = ["POST"])
@@ -74,26 +94,50 @@ def parse_req_2():
 
     result = {}
 
-    result["circle_1"] = requestedData["circle_1"]*2 - 1
-    result["square_1"] = requestedData["square_1"]*2 - 1
-    result["triangle_1"] = requestedData["triangle_1"]*2 - 1
-    result["bright_dark_1"] = requestedData["bright_dark_1"]*2 - 1
-    result["soft_sharp_1"] = requestedData["soft_sharp_1"]*2 - 1
-    result["warm_cool_1"] = requestedData["warm_cool_1"]*2 - 1
-    result["simple_complex_1"] = requestedData["simple_complex_1"]*2 - 1
-    result["disorder_inorder_1"] = requestedData["disorder_inorder_1"]*2 - 1
-    result["high_low_1"] = requestedData["high_low_1"]*2 - 1
+    # result["circle_1"] = requestedData["circle_1"]*2 - 1
+    # result["square_1"] = requestedData["square_1"]*2 - 1
+    # result["triangle_1"] = requestedData["triangle_1"]*2 - 1
+    # result["bright_dark_1"] = requestedData["bright_dark_1"]*2 - 1
+    # result["soft_sharp_1"] = requestedData["soft_sharp_1"]*2 - 1
+    # result["warm_cool_1"] = requestedData["warm_cool_1"]*2 - 1
+    # result["simple_complex_1"] = requestedData["simple_complex_1"]*2 - 1
+    # result["disorder_inorder_1"] = requestedData["disorder_inorder_1"]*2 - 1
+    # result["high_low_1"] = requestedData["high_low_1"]*2 - 1
+    # result["random_noise_1"] = requestedData["random_noise_1"]
+
+    result["circle_1"] = requestedData["circle_1"]
+    result["square_1"] = requestedData["square_1"]
+    result["triangle_1"] = requestedData["triangle_1"]
+    result["bright_dark_1"] = requestedData["bright_dark_1"]
+    result["soft_sharp_1"] = requestedData["soft_sharp_1"]
+    result["warm_cool_1"] = requestedData["warm_cool_1"]
+    result["simple_complex_1"] = requestedData["simple_complex_1"]
+    result["disorder_inorder_1"] = requestedData["disorder_inorder_1"]
+    result["high_low_1"] = requestedData["high_low_1"]
     result["random_noise_1"] = requestedData["random_noise_1"]
 
-    result["circle_2"] = requestedData["circle_2"]*2 - 1
-    result["square_2"] = requestedData["square_2"]*2 - 1
-    result["triangle_2"] = requestedData["triangle_2"]*2 - 1
-    result["bright_dark_2"] = requestedData["bright_dark_2"]*2 - 1
-    result["soft_sharp_2"] = requestedData["soft_sharp_2"]*2 - 1
-    result["warm_cool_2"] = requestedData["warm_cool_2"]*2 - 1
-    result["simple_complex_2"] = requestedData["simple_complex_2"]*2 - 1
-    result["disorder_inorder_2"] = requestedData["disorder_inorder_2"]*2 - 1
-    result["high_low_2"] = requestedData["high_low_2"]*2 - 1
+
+    # result["circle_2"] = requestedData["circle_2"]*2 - 1
+    # result["square_2"] = requestedData["square_2"]*2 - 1
+    # result["triangle_2"] = requestedData["triangle_2"]*2 - 1
+    # result["bright_dark_2"] = requestedData["bright_dark_2"]*2 - 1
+    # result["soft_sharp_2"] = requestedData["soft_sharp_2"]*2 - 1
+    # result["warm_cool_2"] = requestedData["warm_cool_2"]*2 - 1
+    # result["simple_complex_2"] = requestedData["simple_complex_2"]*2 - 1
+    # result["disorder_inorder_2"] = requestedData["disorder_inorder_2"]*2 - 1
+    # result["high_low_2"] = requestedData["high_low_2"]*2 - 1
+    # result["random_noise_2"] = requestedData["random_noise_2"]
+
+
+    result["circle_2"] = requestedData["circle_2"]
+    result["square_2"] = requestedData["square_2"]
+    result["triangle_2"] = requestedData["triangle_2"]
+    result["bright_dark_2"] = requestedData["bright_dark_2"]
+    result["soft_sharp_2"] = requestedData["soft_sharp_2"]
+    result["warm_cool_2"] = requestedData["warm_cool_2"]
+    result["simple_complex_2"] = requestedData["simple_complex_2"]
+    result["disorder_inorder_2"] = requestedData["disorder_inorder_2"]
+    result["high_low_2"] = requestedData["high_low_2"]
     result["random_noise_2"] = requestedData["random_noise_2"]
 
     return result
@@ -170,7 +214,7 @@ def img_generator():
         # return str(output.shape)
 
 @app.route("/img_augmentation", methods = ["POST"])
-def img_augmentation():
+def img_augmentation():     
     # get data including random_noise
     data = parse_req_1()
     print(data)
@@ -183,10 +227,10 @@ def img_augmentation():
         # condition input
         condition_vector = []
         for k, v in data.items():
-            condition_vector.append(v)
+            condition_vector.append(v)  
         condition_vector = np.array(condition_vector[:-1] * row_num)
-        condition_input = condition_vector.reshape(row_num, condition_dim)
-
+        condition_input = condition_vector.reshape(row_num, condition_dim) 
+        
         print(noise_input.shape)
         print(condition_input.shape)
 
@@ -199,36 +243,38 @@ def img_augmentation():
         condition_img = graph.get_tensor_by_name("real_img_digit:0")
         img = sess.run(g_output, feed_dict = {noise_img: noise_input, condition_img: condition_input})
         img = [img[0]]
-
+        
         # condition_final_output
         condition_final_output = []
 
         # generate one picture for each change
-        dim_change = [4, 3, 6, 7]
+        dim_change = [4, 3, 6, 7] 
         # sharp1_soft0, index = 4
         lp = 6
         for i in range(lp):
             if i < 3:
-                condition_change = condition_input
+                condition_change = copy.deepcopy(condition_input)
+                print("before change:{}".format(condition_change[0]))
                 for j in range(len(condition_change)):
                     condition_change[j][4] = condition_change[j][4] + (1 - condition_change[j][4])*(3-i) / 3
                 condition_final_output.append(condition_change[0])
+                print("after change:{}".format(condition_change[0]))
                 pic = sess.run(g_output, feed_dict = {noise_img: noise_input, condition_img: condition_change})
                 pic = [pic[0]]
                 img = np.vstack((img, pic))
             else:
-                condition_change = condition_input
+                condition_change = copy.deepcopy(condition_input)
                 for j in range(len(condition_change)):
                     condition_change[j][4] = condition_change[j][4] - (condition_change[j][4] - 0)*(i-2) / 3
                 condition_final_output.append(condition_change[0])
                 pic = sess.run(g_output, feed_dict = {noise_img: noise_input, condition_img: condition_change})
                 pic = [pic[0]]
                 img = np.vstack((img, pic))
-
+        
         # bright0_dark1, index = 3
         for i in range(lp):
             if i < 3:
-                condition_change = condition_input
+                condition_change = copy.deepcopy(condition_input)
                 for j in range(len(condition_change)):
                     condition_change[j][3] = condition_change[j][3] - (condition_change[j][3] - 0)*(3-i) / 3
                 condition_final_output.append(condition_change[0])
@@ -236,7 +282,7 @@ def img_augmentation():
                 pic = [pic[0]]
                 img = np.vstack((img, pic))
             else:
-                condition_change = condition_input
+                condition_change = copy.deepcopy(condition_input)
                 for j in range(len(condition_change)):
                     condition_change[j][3] = condition_change[j][3] + (1 - condition_change[j][3])*(i-2) / 3
                 condition_final_output.append(condition_change[0])
@@ -247,7 +293,7 @@ def img_augmentation():
         # simple0_complex1, index = 6
         for i in range(lp):
             if i < 3:
-                condition_change = condition_input
+                condition_change = copy.deepcopy(condition_input)
                 for j in range(len(condition_change)):
                     condition_change[j][6] = condition_change[j][6] - (condition_change[j][6] - 0)*(3-i) / 3
                 condition_final_output.append(condition_change[0])
@@ -255,18 +301,18 @@ def img_augmentation():
                 pic = [pic[0]]
                 img = np.vstack((img, pic))
             else:
-                condition_change = condition_input
+                condition_change = copy.deepcopy(condition_input)
                 for j in range(len(condition_change)):
                     condition_change[j][6] = condition_change[j][6] + (1 - condition_change[j][6])*(i-2) / 3
                 condition_final_output.append(condition_change[0])
                 pic = sess.run(g_output, feed_dict = {noise_img: noise_input, condition_img: condition_change})
                 pic = [pic[0]]
                 img = np.vstack((img, pic))
-
+        
         # inorder1_disorder0, index = 7
         for i in range(lp):
             if i < 3:
-                condition_change = condition_input
+                condition_change = copy.deepcopy(condition_input)
                 for j in range(len(condition_change)):
                     condition_change[j][7] = condition_change[j][7] + (1 - condition_change[j][7])*(3-i) / 3
                 condition_final_output.append(condition_change[0])
@@ -274,9 +320,9 @@ def img_augmentation():
                 pic = [pic[0]]
                 img = np.vstack((img, pic))
             else:
-                condition_change = condition_input
+                condition_change = copy.deepcopy(condition_input)
                 for j in range(len(condition_change)):
-                    condition_change[j][7] = condition_change[j][7] - (1 - condition_change[j][7])*(3-i) / 3
+                    condition_change[j][7] = condition_change[j][7] - (condition_change[j][7] - 0)*(i-2) / 3
                 condition_final_output.append(condition_change[0])
                 pic = sess.run(g_output, feed_dict = {noise_img: noise_input, condition_img: condition_change})
                 pic = [pic[0]]
@@ -287,9 +333,10 @@ def img_augmentation():
         output = np.hstack((all_input, img))
         np.savetxt("output_3.csv", output, delimiter = ",", fmt = "%.10f")
         return send_file("output_3.csv")
-
+        
         # the test output on postman
         # return str(output.shape)
+    
 
 @app.route("/img_comparison", methods = ["POST"])
 def img_comparison():
@@ -351,7 +398,7 @@ def img_comparison():
         all_input = np.hstack((n_output, c_output))
         # we don not need the first two pictures
         output = np.hstack((all_input, img[2:]))
-        np.savetxt("output_4.csv", output, delimiter = ",", fmt = '%.10f')
+        np.savetxt("output_4.csv", output, delimiter = ",", fmt = "%.10f")
         return send_file("output_4.csv")
             
         # the test output on postman
